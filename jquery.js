17d477a270b3e8def9e70335e57018f99df8e75a
fa <evelyn2night3@gmail.com>
2023-07-03 17:31:44 -0300

    jfjf

--- jquery.js
+++ jquery.js
@@ -1,14 +1,39 @@
-//de forma js
-var div = document.querySelector(".container");
-console.log(div);
-div2.addEventListener("click", function (){
-    alert("hola mouse");
-})
-console.dir(div);
+var div2 = document.querySelector(".container");
+div2.addEventListener("click",ajaxSinJQuery)
+console.dir(div2);
 
-//de forma jquery
 var divj = $(".container");
-divj.on("mouseover", function(){
-    alert("hola mouse jquery");
-})
+divj.on("mouseover",ajaxConJQuery)
 console.log(divj);
+
+function ajaxSinJQuery() {
+    var url = "https://pokeapi.co/api/v2/pokemon/";
+    let tarjetas = document.querySelector("#nPokemon");
+    var objXMLHttpRequest = new XMLHttpRequest();
+
+    objXMLHttpRequest.onreadystatechange = function () {
+      if (objXMLHttpRequest.readyState === 4) {
+        if (objXMLHttpRequest.status === 200) {
+          let json = JSON.parse(objXMLHttpRequest.responseText);
+          tarjetas.data = json;
+          console.log(json);
+        } else {
+          alert("Error Code: " + objXMLHttpRequest.status);
+          alert("Error Message: " + objXMLHttpRequest.statusText);
+        }
+      }
+    };
+    objXMLHttpRequest.open("GET", url);
+    objXMLHttpRequest.send();
+}
+
+function ajaxConJQuery() {
+    var url = "https://pokeapi.co/api/v2/pokemon/";
+    let tarjetas = $("#nPokemon");
+    $.ajax({
+        url: url,
+        success: function( result ) {
+          console.log(result);
+        }
+      });
+}
